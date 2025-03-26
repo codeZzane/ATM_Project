107d18185f3bb0fbaf26bc77079da00110a1b300
Adam Daniel P. Azarcon <adamdaniel2801@gmail.com>
2025-03-26 14:08:50 +0800

    Create AutomatedTellerMachine.py
    Pseudocode added

---
+++ AutomatedTellerMachine.py
@@ -0,0 +1,13 @@
+# Automated Teller Machine (ATM) - My FIrst Project
+# Author: Adam Daniel P. Azarcon PUP BSCPE
+# Date: March 26, 2025
+
+# Ask for CAN and PIN
+# Verify if registered
+# if not, try asking again for 3 times
+# if yes, display account details
+# ask if deposit or withdraw
+# if withdraw, ask for amount and check whether amount is <= account balance
+# if deposit, ask for amount and add it to the account balance
+# display new balance
+# ask if new transaction or end
